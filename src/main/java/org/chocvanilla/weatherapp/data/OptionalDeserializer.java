82b9302c9d7b2da01b249c801cb33839d9469f78
Rhys Van Orsouw <s3542173@student.rmit.edu.au>
2016-04-22 13:55:24 +1000

    Quick code clean up

--- src/main/java/org/chocvanilla/weatherapp/data/OptionalDeserializer.java
+++ src/main/java/org/chocvanilla/weatherapp/data/OptionalDeserializer.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 
 /**
  * Deserializes a JSON field that may contain an invalid value.
+ *
  * @param <T> The type of the field to be deserialized.
  */
 public class OptionalDeserializer<T> implements JsonDeserializer<T> {
@@ -15,19 +16,20 @@ public class OptionalDeserializer<T> implements JsonDeserializer<T> {
 
     /**
      * Create a new instance which attempts to parse JSON fields.
-     * @param parse the function to convert from a String to the desired type
+     *
+     * @param parse  the function to convert from a String to the desired type
      * @param orElse the return value if the conversion fails
      */
-    public OptionalDeserializer(Function<String, T> parse, T orElse){
-        parser = parse;        
+    public OptionalDeserializer(Function<String, T> parse, T orElse) {
+        parser = parse;
         ifUnsuccessful = orElse;
     }
-    
+
     @Override
     public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         try {
             return parser.apply(json.getAsString());
-        } catch (Exception e){
+        } catch (Exception e) {
             return ifUnsuccessful;
         }
     }
