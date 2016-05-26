aace51f227175ec631389bbdb90eef366a34d0af
Adam Thalhammer <s3544305@student.rmit.edu.au>
2016-05-26 15:57:06 +1000

    add lat and long coordinates to weather stations json file

--- src/test/java/org/chocvanilla/weatherapp/data/forecast/ForecastTest.java
+++ src/test/java/org/chocvanilla/weatherapp/data/forecast/ForecastTest.java
@@ -1,17 +1,25 @@
 package org.chocvanilla.weatherapp.data.forecast;
 
+import com.google.gson.Gson;
+import org.chocvanilla.weatherapp.data.observations.WeatherObservations;
+import org.chocvanilla.weatherapp.data.stations.WeatherStation;
+import org.chocvanilla.weatherapp.io.WeatherStationsJSONFile;
 import org.junit.Before;
+
+import java.io.IOException;
 
 public class ForecastTest {
     private ForecastProvider provider;
+    private WeatherStation station;
     
     @Before
-    private void setUp(){
+    private void setUp() throws IOException {
         provider = new ForecastIO();
+        station = new WeatherStationsJSONFile(new Gson()).load().get(0);
     }
     
     private void shouldRetrieveForecastForCoordinates(){
-        provider.getForecast();
+        WeatherObservations forecast = provider.getForecast(station);
     }
     
 }
