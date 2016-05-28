4e618363a67e6f48cd6bf76b5ff242f714ea6ee1
Raj Adhikari <peez@localhost.com>
2016-05-28 20:11:17 +1000

    refactor

--- src/test/java/org/chocvanilla/weatherapp/data/forecast/io/WeatherStationJSONFileTest.java
+++ src/test/java/org/chocvanilla/weatherapp/data/forecast/io/WeatherStationJSONFileTest.java
@@ -1,6 +1,7 @@
 package org.chocvanilla.weatherapp.data.forecast.io;
 
 import com.google.gson.Gson;
+import org.chocvanilla.weatherapp.data.stations.WeatherStation;
 import org.chocvanilla.weatherapp.data.stations.WeatherStations;
 import org.chocvanilla.weatherapp.io.WeatherStationSource;
 import org.chocvanilla.weatherapp.io.WeatherStationsJSONFile;
@@ -9,25 +10,34 @@ import org.junit.Test;
 import org.junit.BeforeClass;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
-public class WeatherStationJSONFileTeset {
+public class WeatherStationJSONFileTest {
     private final Gson gson = new Gson();
+    private WeatherStationsJSONFile source;
 
-
-    @Test
-    public void loadFromJSON() {
-
+    @Before
+    public void setUp() {
+        source = new WeatherStationsJSONFile(gson);
     }
 
     @Test
+    public void testLoad() throws IOException{
+        List<WeatherStation> ws = source.load();
+        assertNotNull(ws);
+    }
+
+
+    @Test
     public void weatherStationsJSONFileIsPresent() throws IOException {
-        WeatherStationSource source = new WeatherStationsJSONFile(gson);
         WeatherStations stations = WeatherStations.loadFrom(source);
         assertThat(stations, is(not(empty())));
     }
