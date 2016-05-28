6f060fe17bc58df842ba069f6670464928115b22
Raj Adhikari <peez@localhost.com>
2016-05-29 03:29:13 +1000

    Added BOM class test

---
+++ src/test/java/org/chocvanilla/weatherapp/data/observations/BureauOfMeteorologyTest.java
@@ -0,0 +1,34 @@
+package org.chocvanilla.weatherapp.data.observations;
+
+import com.google.gson.Gson;
+import org.chocvanilla.weatherapp.data.stations.WeatherStation;
+import org.chocvanilla.weatherapp.data.stations.WeatherStations;
+import org.chocvanilla.weatherapp.io.WeatherStationSource;
+import org.chocvanilla.weatherapp.io.WeatherStationsJSONFile;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BureauOfMeteorologyTest {
+    private BureauOfMeteorology bom;
+
+    @Before
+    public void setUp(){
+        bom = new BureauOfMeteorology(new Gson());
+    }
+
+    @Test
+    public void loadObservationsTest() throws IOException {
+        WeatherStationSource source = new WeatherStationsJSONFile(new Gson());
+        WeatherStations ws = WeatherStations.loadFrom(source);
+        WeatherStation s = ws.stream()
+                .findAny()
+                .get();
+        WeatherObservations ob =  bom.loadObservations(s);
+
+        assertNotNull(ob);
+    }
+}
