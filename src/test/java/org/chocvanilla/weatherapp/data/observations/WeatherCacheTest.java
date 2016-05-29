13e8504c5f3cf401dd4089d58ec0448713e52b78
Raj Adhikari <peez@localhost.com>
2016-05-29 16:28:44 +1000

    Added cache expiry test

--- src/test/java/org/chocvanilla/weatherapp/data/observations/WeatherCacheTest.java
+++ src/test/java/org/chocvanilla/weatherapp/data/observations/WeatherCacheTest.java
@@ -7,31 +7,57 @@ import org.chocvanilla.weatherapp.data.stations.WeatherStations;
 import org.chocvanilla.weatherapp.io.WeatherStationSource;
 import org.chocvanilla.weatherapp.io.WeatherStationsJSONFile;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.IsNot.not;
+
 public class WeatherCacheTest {
     private WeatherCache cache;
+    private static BureauOfMeteorology bom;
+    private static WeatherStationSource source;
+    private static WeatherStations ws;
+    private static WeatherStation s;
+
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        bom = new BureauOfMeteorology(DataHelpers.observationsGson());
+        source = new WeatherStationsJSONFile(new Gson());
+        ws = WeatherStations.loadFrom(source);
+        s = ws.stream()
+                .findAny()
+                .get();
+
+    }
 
     @Before
     public void setUp() throws IOException{
-        WeatherStationSource source = new WeatherStationsJSONFile(new Gson());
-        WeatherStations ws = WeatherStations.loadFrom(source);
-        WeatherStation s = ws.stream()
-                .findAny()
-                .get();
-        BureauOfMeteorology bom = new BureauOfMeteorology(DataHelpers.observationsGson());
-
         cache = new WeatherCache(s, bom);
     }
 
     @Test
     public void getCachedObservations() {
+        WeatherObservations obs = cache.get();
+        assertThat(obs, is(not(empty())));
+    }
+
+    @Test
+    public void cacheExpiredTest () {
+
+        cache = new WeatherCache(s, bom) {
+            @Override
+            public long msSinceLastRefreshed() {
+                return TimeUnit.MINUTES.toMillis(5) + 1;
+            }
+        };
+
         WeatherObservations obs = cache.get();
         assertThat(obs, is(not(empty())));
     }
